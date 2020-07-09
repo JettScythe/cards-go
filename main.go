9bd04e2f97ed0c21db48318d810d59c2013d1e10
JettScythe <jettscythe@hotmail.com>
2020-07-09 01:41:54 -0300

    Added comments & deal()

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 func main() {
+	// create a new deck
 	cards := newDeck()
+	// shuffle the deck
 	cards.shuffle()
-	cards.print()
+	// deal a hand of 5 cards
+	hand, _ := deal(cards, 5)
+	// print cards to console
+	hand.print()
 }

ee7a6c6465eb5920be834a1ce596fd10f4314fe7
JettScythe <jettscythe@hotmail.com>
2020-07-17 20:45:05 -0300

    updated testing functions

--- deck_test.go
+++ deck_test.go
@@ -6,21 +6,23 @@ import (
 )
 
 func TestNewDeck(t *testing.T) {
-	d := newDeck()
+	d := new()
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52 but got %v", len(d))
 	}
-	if d[0] != "Ace of Spades" {
-		t.Errorf("Expected Ace of Spaces, but got %v", d[0])
+	firstCard := Card{"Ace", "Spades"}
+	if d[0] != firstCard {
+		t.Errorf("Expected Ace Spades but got %v", d[0])
 	}
-	if d[len(d)-1] != "King of Clubs" {
-		t.Errorf("Expected King of Clubs but got %v", d[len(d)-1])
+	lastCard := Card{"King", "Clubs"}
+	if d[len(d)-1] != lastCard {
+		t.Errorf("Expected King Clubs but got %v", d[len(d)-1])
 	}
 }
 
 func TestSaveToFileandNewDeckFromFile(t *testing.T) {
 	os.Remove("_decktesting")
-	deck := newDeck()
+	deck := new()
 	deck.saveToFile("_decktesting")
 	loadedDeck := newDeckFromFile("_decktesting")
 	if len(loadedDeck) != 52 {
